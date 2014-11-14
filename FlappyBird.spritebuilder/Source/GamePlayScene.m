279cd47cb1f03e4e5b9c5c202643d6cc35934dce
Tiffy <Tiffy@Tiffys-Mac-Pro.local>
2014-11-13 22:13:01 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,13 +9,29 @@
     // your code here
     character = (Character *)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
+    //Increment the time since the last obstacle was added
+    timeSinceObstacle += delta;//delta is 1/60 a sec
+    
+    /*Check to see if two seconds have passed*/
+    if (timeSinceObstacle > 2.0f){
+        [self addObstacle];
+        
+    //reset
+    timeSinceObstacle = 0.0f;
+    }
     // put update code here
 }
 
 // put new methods here
+-(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    [character flap];
+}
 
 @end
